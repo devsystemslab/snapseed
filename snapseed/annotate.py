96323300740108a6f2df9a89689a8b4e0187d2a3
joschif <jonas.simon.fleck@gmail.com>
2023-03-10 21:33:27 +0100

    Raise error if unknown method

--- snapseed/annotate.py
+++ snapseed/annotate.py
@@ -8,6 +8,8 @@ def annotate(adata, marker_dict, group_name, method="snap", layer=None):
         assignments = annotate_snap(adata, marker_dict, group_name, layer=layer)
     if method == "trinatize":
         assignments = annotate_cytograph(adata, marker_dict, group_name)
+    else:
+        raise ValueError("Unknown method.")
     # Join cluster-level results with adata
     assignments = assignments.reset_index(names=group_name)
     return assignments
